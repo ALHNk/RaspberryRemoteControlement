f7c9c1c0309d4d900ac3831411f7a8673819ecfa
ALHNk <khamidulla.alikhan@gmail.com>
2026-02-13 17:22:47 +0500

    Emergency Stop Modified

--- TCP/tcp.c
+++ TCP/tcp.c
@@ -54,7 +54,8 @@ void emergancy_stop()
 {
     setGoalSpeed(0, 2, MOTOR_TYPE);
     setGoalSpeed(0, 3, MOTOR_TYPE);
-
+    disconnect_all_motors();
+    closeMotorPort();
     printf("Emergency Stop");
     exit(1);
 }
